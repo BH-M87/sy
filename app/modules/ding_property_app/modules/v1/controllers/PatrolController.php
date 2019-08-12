61a259b0e1ce3ec65a8a99caa890c0f01bbf4c73
zhangqiang <devzq@zhujia360.com>
2019-08-12 16:20:22 +0800

    zq123

---
+++ app/modules/ding_property_app/modules/v1/controllers/PatrolController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangqiang
+ * Date: 2019-08-12
+ * Time: 14:52
+ */
+
+namespace app\modules\ding_property_app\modules\v1\controllers;
+
+
+class PatrolController
+{
+
+}
---
+++ app/modules/property/modules/v1/controllers/PatrolController.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangqiang
+ * Date: 2019-08-12
+ * Time: 14:51
+ */
+
+namespace app\modules\property\modules\v1\controllers;
+
+
+use app\modules\property\controllers\BaseController;
+
+class PatrolController extends BaseController
+{
+    /*         巡更点相关接口                         */
+    //巡更点列表
+    public function actionPointList()
+    {
+
+    }
+
+    //巡更点新增
+    public function actionPointAdd()
+    {
+
+    }
+
+    //巡更点编辑
+    public function actionPointEdit()
+    {
+
+    }
+
+    //巡更点删除
+    public function actionPointDel()
+    {
+
+    }
+
+    //巡更点详情
+    public function actionPointDetail()
+    {
+
+    }
+
+    //巡更点是否需要定位
+    public function actionGetLocation()
+    {
+
+    }
+
+    //巡更点是否需要拍照
+    public function actionGetPhone()
+    {
+
+    }
+
+    //巡更点未选择列表
+    public function actionPointListUnchoose()
+    {
+
+    }
+
+    //巡更点已选择列表
+    public function actionPointListChoose()
+    {
+
+    }
+
+    /*         巡更线路接口                            */
+    //巡更线路列表
+    public function actionLineList()
+    {
+
+    }
+
+    //巡更线路新增
+    public function actionLineAdd()
+    {
+
+    }
+
+    //巡更线路编辑
+    public function actionLineEdit()
+    {
+
+    }
+
+    //巡更线路删除
+    public function actionLineDel()
+    {
+
+    }
+
+    //巡更线路详情
+    public function actionLineDetail()
+    {
+
+    }
+
+    /*            巡更计划接口                      */
+    //巡更计划列表
+    public function actionPlanList()
+    {
+
+    }
+
+    //巡更计划新增
+    public function actionPlanAdd()
+    {
+
+    }
+
+    //巡更计划编辑
+    public function actionPlanEdit()
+    {
+
+    }
+
+    //巡更计划删除
+    public function actionPlanDel()
+    {
+
+    }
+
+    //巡更计划详情
+    public function actionPlanDetail()
+    {
+
+    }
+
+    //巡更计划 获取该小区下所有有效的执行人员
+    public function actionPlanUserList()
+    {
+
+    }
+
+    /*              巡更记录接口                */
+    //巡更记录列表
+    public function actionRecordList()
+    {
+
+    }
+
+    //巡更记录详情
+    public function actionRecordDetail()
+    {
+
+    }
+
+    //巡更记录导出
+    public function actionRecordExport()
+    {
+
+    }
+
+    //计划线路，筛选用
+    public function actionRecordLineList()
+    {
+
+    }
+
+    //计划列表，筛选用
+    public function actionRecordPlanList()
+    {
+
+    }
+
+    //巡更状态
+    public function actionRecordStatus()
+    {
+
+    }
+
+    //对应路线
+    public function actionRecordToLine()
+    {
+
+    }
+
+    //计划类型
+    public function actionRecordPlanType()
+    {
+
+    }
+
+    /*         统计报表                 */
+    //巡更统计数据
+    public function actionReportIndex()
+    {
+
+    }
+
+    //旷巡排行榜
+    public function actionReportRank()
+    {
+
+    }
+
+
+}
