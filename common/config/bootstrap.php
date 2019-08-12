1e1d13de29a7327471e3d880dfffedf4186fe50f
冯文超 <fengwenchao@zhujia360.com>
2019-08-12 17:22:06 +0800

    底层代码修改

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -2,3 +2,4 @@
 Yii::setAlias('@common', dirname(dirname(__DIR__)) . '/common');
 Yii::setAlias('@app', dirname(dirname(__DIR__)) . '/app');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@service', dirname(dirname(__DIR__)) . '/service');
