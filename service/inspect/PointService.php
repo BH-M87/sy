48d704cae8fb22b4cb529b22ab9e9d09a2dd0898
wyf <Wyf186038>
2019-08-12 10:54:29 +0800

    feat:1.巡检部分业务代码

---
+++ service/inspect/PointService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: wyf
+ * Date: 2019/8/12
+ * Time: 10:42
+ */
+namespace service\inspect;
+
+use service\BaseService;
+
+class PointService extends BaseService
+{
+
+}
