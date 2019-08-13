61a5c2e79f33c5e6bd7a5c1328884ae092fb0d9c
chenkelang <chenkelang@zhujia360.com>
2019-08-13 10:34:30 +0800

    1

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 Yii::setAlias('@common', dirname(dirname(__DIR__)) . '/common');
-Yii::setAlias('@app', dirname(dirname(__DIR__)) . '/app');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@app', dirname(dirname(__DIR__)) . '/app');
