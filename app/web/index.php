8ab26f964e31c715328c5a6b02fb40806adc757a
chenkelang <chenkelang@zhujia360.com>
2019-08-24 17:57:43 +0800

    Yii依赖

--- app/web/index.php
+++ app/web/index.php
@@ -9,17 +9,16 @@ if (empty($envData['YII_ENV']) || !isset($envData['YII_DEBUG']) || !in_array($en
 }
 defined('YII_DEBUG') or define('YII_DEBUG', (!empty($envData['YII_ENV']) ? (bool)$envData['YII_ENV'] : false));
 defined('YII_ENV') or define('YII_ENV', $envData['YII_ENV']);
-defined('YII_OWNER') or define('YII_OWNER', $envData['OWNER']);
 
-require __DIR__ . '/../../vendor/autoload.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/../../common/config/bootstrap.php';
-require __DIR__ . '/../config/bootstrap.php';
+require __DIR__ . '/../../../../vendor/autoload.php';
+require __DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../../../../common/config/bootstrap.php';
+require __DIR__ . '/../../../../app/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../../../common/config/main.php',
     require __DIR__ . '/../../common/config/main-dev.php',
-    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../../../../app/config/main.php',
     require __DIR__ . '/../config/main-dev.php'
 );
 
