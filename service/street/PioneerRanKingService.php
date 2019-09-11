29c8bf24524cfb224212d601f301eaa4c56eefea
Yejianhong <yejianhong@zhujia360.com>
2019-09-11 10:49:58 +0800

    'aa'

--- service/street/PioneerRanKingService.php
+++ service/street/PioneerRanKingService.php
@@ -122,7 +122,8 @@ class PioneerRanKingService extends BaseService
         $params['end'] = strtotime($params['years'].'-12-31 24:00');
         $data = StPartyTaskStation::getOrderList($params);
         $user_top = StPartyTaskStation::getUserTop($user['communist_id']);
-        return [$data,'user' => $user_top[0] ?? null];
+        $data['user'] = $user_top ?? null;
+        return $data;
     }
 
     /**
