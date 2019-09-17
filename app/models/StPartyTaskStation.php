2ea8dffd8266fa0bb721873820602579cf4e80ff
Yejianhong <yejianhong@zhujia360.com>
2019-09-17 14:40:24 +0800

    'aa'

--- app/models/StPartyTaskStation.php
+++ app/models/StPartyTaskStation.php
@@ -69,6 +69,7 @@ class StPartyTaskStation extends \yii\db\ActiveRecord
     {
         $model = self::find()->filterWhere(['status' => $param['status'] ?? null])
             ->andFilterWhere(['communist_id' => $param['communist_id'] ?? null])
+            ->andFilterWhere(['id' => $param['id'] ?? null])
             ->andFilterWhere(['<=','create_at',$param['end'] ?? null])
             ->andFilterWhere(['>=','create_at',$param['start'] ?? null]);
         $model->orderBy([ 'create_at' => SORT_DESC]);
