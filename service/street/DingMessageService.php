7cc6f529fdae4eab1d64310923b5abef11ebd025
Zhang Qiang <devzq@zhujia360.com>
2019-09-18 17:08:15 +0800

    zq

--- service/street/DingMessageService.php
+++ service/street/DingMessageService.php
@@ -23,13 +23,10 @@ class DingMessageService extends BaseService
         $departName = UserService::service()->getDepartmentNameByCode($organization_id);
         //给这些未读的对象发送钉钉消息
         $sendData['title'] = '通知通报';
-        $markdown = "**通知通报**"."
-        
-        ".$title."
-        
-        ".$departName."/".$operator_name."
-        
-        "."提醒时间：".date("Y-m-d H:i:s");
+        $markdown = "**通知通报**";
+        $markdown .= $title;
+        $markdown .= $departName."/".$operator_name;
+        $markdown .= "提醒时间：".date("Y-m-d H:i:s");
         $sendData['markdown'] = $markdown;
         $sendData['single_title'] = "查看详情";
         //$query = urlencode("id=".$id);
