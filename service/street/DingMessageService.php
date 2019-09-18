10b043b8cc60ba144b07571e0a876430ce288659
Zhang Qiang <devzq@zhujia360.com>
2019-09-18 19:23:18 +0800

    3

--- service/street/DingMessageService.php
+++ service/street/DingMessageService.php
@@ -30,9 +30,9 @@ class DingMessageService extends BaseService
         $markdown .= $departName."/".$operator_name.$br;
         $markdown .= "提醒时间：".date("Y-m-d H:i:s");*/
         $markdown = "#### **通知通报**";
-        $markdown .= "##### ".$title."
-        ".$departName."/".$operator_name."
-        "."提醒时间：".date("Y-m-d H:i:s");
+        $markdown .= $title."
+".$departName."/".$operator_name."
+"."提醒时间：".date("Y-m-d H:i:s");
         $sendData['markdown'] = $markdown;
         $sendData['single_title'] = "查看详情";
         //$query = urlencode("id=".$id);
