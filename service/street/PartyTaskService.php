53bd936a22d71ac30e54b624978eeca5a9cb9de7
Yejianhong <yejianhong@zhujia360.com>
2019-09-20 14:35:51 +0800

    'aa'

--- service/street/PartyTaskService.php
+++ service/street/PartyTaskService.php
@@ -287,7 +287,7 @@ class PartyTaskService extends BaseService
         $avg_value = $model->where(['sts.status' => 3])->andWhere(['st.organization_type' => $param['organization_type'],'st.organization_id' => $param['organization_id']])->sum('sts.pioneer_value');
         $data['no_audited'] = $model->where(['sts.status' => 2])->andWhere(['st.organization_type' => $param['organization_type'],'st.organization_id' => $param['organization_id']])->count();
         $data['audited'] =$model->where(['sts.status' => 3])->andWhere(['st.organization_type' => $param['organization_type'],'st.organization_id' => $param['organization_id']])->count();
-        $data['value'] = empty($value) ? '0' : number_format($value/ ($data['no_audited'] + $data['audited']),1);
+        $data['value'] = empty($value) ? '0' : number_format($value/ $data['audited'],1);
         $data['avg_value'] = empty($avg_value) ? '0' : number_format($avg_value / $data['audited'],1);
         return $data;
     }
