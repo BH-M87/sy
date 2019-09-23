453808aa13a29711eba77224deeb3f88931b463c
Yejianhong <yejianhong@zhujia360.com>
2019-09-23 19:34:38 +0800

    'aa'

--- backend/controllers/CompanyH5Controller.php
+++ backend/controllers/CompanyH5Controller.php
@@ -6,6 +6,7 @@
  * Time: 11:19
  */
 namespace backend\controllers;
+use backend\models\PsCommunityModel;
 use common\core\F;
 use common\MyException;
 use Yii;
@@ -44,6 +45,10 @@ class CompanyH5Controller extends Controller
         } else {
             $params = $_POST;
             $tran = \Yii::$app->getDb()->beginTransaction();
+            $company = PsPropertyCompany::find()->where(['alipay_account' => $params['alipay_account']])->one();
+            if ($company) {
+                die('该支付宝账户已存在');
+            }
             try {
                 $company = new PsPropertyCompany();
                 $company->property_name = $params['enterprise_name'];
@@ -79,6 +84,36 @@ class CompanyH5Controller extends Controller
                 die('新增失败');
             }
             $this->redirect('index');
+        }
+    }
+
+    public function actionBound()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $company = PsPropertyCompany::find()->alias('ppc')
+                ->innerJoin('ps_property_alipay as ppa','ppa.company_id = ppc.id')->where(['ppa.status' => 2])->all();
+            return $this->render('community', [
+                'company' => $company,
+            ]);
+
+        } else {
+            $params = $_POST;
+            $communitys = explode(',',$params['communitys']);
+            $data = [];
+            foreach ($communitys as $v) {
+                $community = PsCommunityModel::find()->where(['id' => $v])->one();
+                if (empty($community->pro_company_id)) {
+                    $community->pro_company_id = $params['pro_id'];
+                    $community->save();
+                } else {
+                    $data[] = $v;
+                }
+            }
+            if (!empty($data)) {
+                die('小区ID: '.implode(',',$data).' 已绑定过物业公司!!!');
+            } else {
+                $this->redirect('index');
+            }
         }
     }
 
