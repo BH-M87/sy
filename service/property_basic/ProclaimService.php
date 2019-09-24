4690f8a2bcea81a1d46141b53a4b8d5c8e2454a4
wujianyang <wujianyang@zhujia360.com>
2019-09-24 15:50:13 +0800

    公告

--- service/property_basic/ProclaimService.php
+++ service/property_basic/ProclaimService.php
@@ -236,7 +236,7 @@ class ProclaimService extends BaseService
                 ->filterWhere(['=', 'B.community_id', $p['community_id']])
                 ->orFilterWhere(['=', 'A.community_id', $p['community_id']])
                 ->andWhere(['A.is_show' => 2])
-                ->orderBy('A.show_at desc')->asArray()->all();
+                ->orderBy('is_top desc, top_at desc, show_at desc')->asArray()->all();
             if (!empty($proclaim)) {
                 foreach ($proclaim as &$p) {
                     $p['show_at'] = !empty($p['show_at']) ? date('Y-m-d H:i', $p['show_at']) : '';
