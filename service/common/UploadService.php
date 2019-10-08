eb8ef8d994aa74129ef2bed9c73c384b562cf27f
冯文超 <fengwenchao@zhujia360.com>
2019-10-08 13:52:49 +0800

    图片上传优化

--- service/common/UploadService.php
+++ service/common/UploadService.php
@@ -76,7 +76,55 @@ class UploadService extends BaseService
             return $this->failed('本地保存失败');
         }
         chmod($newFile, 0755);
+        $this->image_png_size_add($newFile,$newFile);
         return $this->success(['fileName' => $newFileName, 'fileDir' => $realDir, 'parentDir' => $parentDir]);
+    }
+
+    public function image_png_size_add($imgsrc,$imgdst){
+        list($width,$height,$type) = getimagesize($imgsrc);
+        $new_width = ($width>600?600:$width)*0.9;
+        $new_height =($height>600?600:$height)*0.9;
+        switch($type){
+            case 1:
+                $giftype= $this->check_gifcartoon($imgsrc);
+                if($giftype){
+                    header('Content-Type:image/gif');
+                    $image_wp=imagecreatetruecolor($new_width, $new_height);
+                    $image = imagecreatefromgif($imgsrc);
+                    imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                    imagejpeg($image_wp, $imgdst,75);
+                    imagedestroy($image_wp);
+                }
+                break;
+            case 2:
+                header('Content-Type:image/jpeg');
+                $image_wp=imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefromjpeg($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                imagejpeg($image_wp, $imgdst,75);
+                imagedestroy($image_wp);
+                break;
+            case 3:
+                header('Content-Type:image/png');
+                $image_wp=imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefrompng($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                imagejpeg($image_wp, $imgdst,75);
+                imagedestroy($image_wp);
+                break;
+        }
+    }
+
+    /**
+     * desription 判断是否gif动画
+     * @param sting $image_file图片路径
+     * @return boolean t 是 f 否
+     */
+    public function check_gifcartoon($image_file){
+        $fp = fopen($image_file,'rb');
+        $image_head = fread($fp,1024);
+        fclose($fp);
+        return preg_match("/".chr(0x21).chr(0xff).chr(0x0b).'NETSCAPE2.0'."/",$image_head)?false:true;
     }
 
     /**
