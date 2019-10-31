8a9a44463411c22709f83049a3a2ae3e08aafe4b
Zhang Qiang <devzq@zhujia360.com>
2019-10-31 17:44:44 +0800

    zq

---
+++ app/modules/street/modules/v1/controllers/CarDataController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: ZQ
+ * Date: 2019/10/31
+ * Time: 14:54
+ * For: 一车一档
+ */
+
+namespace app\modules\street\modules\v1\controllers;
+
+
+class CarDataController extends BaseController
+{
+    public function actionList()
+    {
+
+    }
+
+    public function actionDetail()
+    {
+
+    }
+
+    public function actionDetailInfo()
+    {
+
+    }
+
+    public function actionDayReport()
+    {
+
+    }
+
+    public function actionDayDetail()
+    {
+
+    }
+
+}
