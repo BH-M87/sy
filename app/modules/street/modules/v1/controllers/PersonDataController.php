f9694677f03e8171c0075063bca1ef7a98093c20
fengwenchao@zhujia360.com <fengwenchao@zhujia360.com>
2019-10-31 14:27:55 +0800

    接口提交

---
+++ app/modules/street/modules/v1/controllers/PersonDataController.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * 一人一档数据
+ * User: wenchao.feng
+ * Date: 2019/10/31
+ * Time: 11:32
+ */
+
+namespace app\modules\street\modules\v1\controllers;
+
+
+class PersonDataController extends BaseController
+{
+    //列表
+    public function actionList()
+    {
+
+    }
+
+    //详情
+    public function actionView()
+    {
+
+    }
+
+    //公共接口
+    public function actionGetCommon()
+    {
+
+    }
+
+    //人行记录
+    public function actionAcrossDayReport()
+    {
+
+    }
+
+    //人行记录每天详情
+    public function actionAcrossDayDetail()
+    {
+
+    }
+
+    //人行记录规律图
+    public function actionAcrossLineStatistic()
+    {
+
+    }
+
+    //关联家人
+    public function actionRelatedFamily()
+    {
+
+    }
+
+    //关联访客
+    public function actionRelatedVisitor()
+    {
+
+    }
+
+    //关联车辆
+    public function actionRelatedCar()
+    {
+
+    }
+
+}
