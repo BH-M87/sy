473f6d98a030e5f6310ec067e9a3532bf3b979f7
Zhang Qiang <devzq@zhujia360.com>
2019-11-09 13:48:35 +0800

    zq

--- app/commands/TmpController.php
+++ app/commands/TmpController.php
@@ -3,6 +3,7 @@
 
 namespace app\commands;
 
+use app\models\DoorRecord;
 use app\models\ParkingAcross;
 use common\core\F;
 use common\core\PsCommon;
@@ -12,17 +13,43 @@ use yii\console\Controller;
 Class TmpController extends Controller
 {
 
+    const RECORD_SYNC_DOOR = "record_sync_door";//人行出入记录同步
     const RECORD_SYNC_CAR = "record_sync_car";//车行出入记录同步
     public function actionTest()
     {
         file_put_contents('./1.txt',time());
     }
 
-    //用不小区住户数据
+    //同步人行出入记录到redis
+    public function actionSyncRecordDoor($day ='')
+    {
+        if($day){
+            $start_time = strtotime($day." 00:00:00");
+            $end_time = strtotime($day." 23:59:59");
+            $flag = true;
+            $page = 1;
+            $pageSize = 1000;
+            while($flag){
+                $offset = ($page-1)*$pageSize;
+                $limit = $pageSize;
+                $list = DoorRecord::find()->where(['>=','open_time',$start_time])->andFilterWhere(['<=','open_time',$end_time])->limit($limit)->offset($offset)->asArray()->all();
+                if($list){
+                    echo $page;
+                    foreach($list as $key=>$value){
+                        Yii::$app->redis->rpush(YII_PROJECT.YII_ENV.self::RECORD_SYNC_DOOR,json_encode($value));
+                    }
+                    $page ++;
+                }else{
+                    $flag = false;
+                }
+            }
+        }
+
+    }
+
+    //同步车行出入记录到redis
     public function actionSyncRecordCar($day = '')
     {
-        //$request = F::request();//住户传入数据
-        //$day = PsCommon::get($request,"day");
         if($day){
             $start_time = strtotime($day." 00:00:00");
             $end_time = strtotime($day." 23:59:59");
