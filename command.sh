05cb037ad7fbf02d788e52a7cbf8675eec2f8fab
zhangqiang <devzq@zhujia360.com>
2019-11-15 16:32:32 +0800

    zq

--- command.sh
+++ command.sh
@@ -6,6 +6,10 @@ for (( i = 0; i < 60; i=(i+step) )); do
     $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-hf command/iot-data)
     $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-test-fy command/iot-data)
     $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-test-hf command/iot-data)
+    $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-fy command/iot-face)
+    $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-hf command/iot-face)
+    $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-test-fy command/iot-face)
+    $(/usr/local/php/bin/php /data/fczl-backend/www/api_basic_sqwn/yii-test-hf command/iot-face)
     sleep $step
 done
 
