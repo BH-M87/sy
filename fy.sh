50d7a86b75fe34b822e4c24987885a19ea627583
Zhang Qiang <devzq@zhujia360.com>
2019-11-19 10:00:56 +0800

    1

--- fy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-step=3 #间隔的秒数，不能大于60
-
-for (( i = 0; i < 60; i=(i+step) )); do
-    $(/usr/local/bin/docker-compose -f /data/fczl-backend/docker-compose.yml exec -T php-fpm php /var/www/api/yii command/iot-data)
-    sleep $step
-done
-
-exit 0
