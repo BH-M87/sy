980a800d2d55ca7ceb9453b090df59268fe18da8
chenkelang <chenkelang@zhujia360.com>
2019-12-12 16:58:06 +0800

    1

--- app/config/params-project-config.php
+++ app/config/params-project-config.php
@@ -9,14 +9,14 @@ $paramsConfig['dev'] = [
 ];
 //线上测试配置
 $paramsConfig['test'] = [
-    'host_name' => '',       //物业自己的接口域名
+    'host_name' => 'https://sy-wy-pre.zje.com/',       //物业自己的接口域名
     'javaUrl' => '',    //java的接口域名
     'javaAppKey' => 'community-property',                   //java的appKey
     'javaAppSecret' => 'cMchTBCquBl3IWnHmt07i4pVSTXB18rqWR',//java的appSecret
 ];
 //线上生产配置
 $paramsConfig['prod'] = [
-    'host_name' => '',       //物业自己的接口域名
+    'host_name' => 'https://sy-wy.zje.com/',       //物业自己的接口域名
     'javaUrl' => ' ',    //java的接口域名
     'javaAppKey' => 'community-property',                   //java的appKey
     'javaAppSecret' => 'cMchTBCquBl3IWnHmt07i4pVSTXB18rqWR',//java的appSecret
