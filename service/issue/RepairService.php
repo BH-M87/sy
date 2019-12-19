992cc1aa53723725ae2c646b1d7a0e5d08b774b9
wujianyang <wujianyang@zhujia360.com>
2019-12-19 16:06:00 +0800

    no message

--- service/issue/RepairService.php
+++ service/issue/RepairService.php
@@ -181,7 +181,8 @@ class RepairService extends BaseService
         }
 
         if ($memberName) {
-            $query->andWhere(['or', ['like', 'A.contact_name', $memberName ], ['like', 'A.contact_mobile', $memberName]]);
+            $query->andWhere(['like', 'A.operator_name', $memberName ]);
+            //$query->andWhere(['or', ['like', 'A.contact_name', $memberName ], ['like', 'A.contact_mobile', $memberName]]);
         }
         if ($memberMobile) {
             $query->andWhere(['like', 'A.contact_mobile', $memberMobile]);
