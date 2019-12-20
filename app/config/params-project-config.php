9dd755c07c7963d3eebbfd5222b06f47c6312cbc
chenkelang <chenkelang@zhujia360.com>
2019-12-20 18:40:57 +0800

    1

--- app/config/params-project-config.php
+++ app/config/params-project-config.php
@@ -15,7 +15,7 @@ $paramsConfig['test'] = [
     'javaAppSecret' => 'cMchTBCquBl3IWnHmt07i4pVSTXB18rqWR',//java的appSecret
 ];
 //线上生产配置
-$paramsConfig['prod'] = [
+$paramsConfig['master'] = [
     'host_name' => 'https://sy-wy.zje.com/',       //物业自己的接口域名
     'javaUrl' => ' ',    //java的接口域名
     'javaAppKey' => 'community-property',                   //java的appKey
