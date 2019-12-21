39ca8ad4e2100727e43fa5f83bcbfca306cd648f
chenkelang <chenkelang@zhujia360.com>
2019-12-21 15:27:37 +0800

    1

--- app/modules/ding_property_app/controllers/UserBaseController.php
+++ app/modules/ding_property_app/controllers/UserBaseController.php
@@ -4,6 +4,7 @@ namespace app\modules\ding_property_app\controllers;
 use app\modules\ding_property_app\services\UserService;
 
 use common\core\F;
+use common\core\JavaCurl;
 use service\property_basic\JavaOfCService;
 
 class UserBaseController extends BaseController
@@ -18,7 +19,13 @@ class UserBaseController extends BaseController
         if(!parent::beforeAction($action)) return false;
 
         // 查找用户的信息
-        $userInfo = JavaOfCService::service()->memberBase(['token' => $this->token]);
+        //todo::调用java token鉴权接口,并拿到用户信息
+        $params = [
+            'route' => '/user/validate-token',
+            'token' => $this->token,
+        ];
+        //todo::调用java接口
+        $userInfo = JavaCurl::getInstance()->pullHandler($params);
 
         $this->userInfo = $userInfo;
         $this->userInfo['mobile'] = $userInfo['sensitiveInf'];
