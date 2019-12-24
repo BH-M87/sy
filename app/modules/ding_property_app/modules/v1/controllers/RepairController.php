d035cb5498e52f82d9fd4b46e77eb9bce77651cb
wujianyang <wujianyang@zhujia360.com>
2019-12-24 14:40:16 +0800

    no message

--- app/modules/ding_property_app/modules/v1/controllers/RepairController.php
+++ app/modules/ding_property_app/modules/v1/controllers/RepairController.php
@@ -84,7 +84,6 @@ class RepairController extends UserBaseController
         $p['repair_content'] = F::value($this->params, 'repair_content', '');
         $p['contact_mobile'] = F::value($this->params, 'contact_mobile', '');
         $p['repair_imgs'] =  F::value($this->params, 'repair_imgs', '');
-        $p['expired_repair_time'] = $p['expired_repair_time'] ? date("Y-m-d", $p['expired_repair_time']) : 0;
         $p['repair_from'] = 3;
         $p['token'] = F::value($this->params, 'token', '');
 
