ed370ebff6d3e1aed86a3c0bf5f32ffc3047c1b0
zhouph <zhoupenghui@zhujia360.com>
2019-12-26 16:59:08 +0800

    c端添加会员积分

--- app/modules/ali_small_lyl/modules/v1/controllers/VoteController.php
+++ app/modules/ali_small_lyl/modules/v1/controllers/VoteController.php
@@ -8,6 +8,7 @@ use common\core\PsCommon;
 use app\models\PsAppUser;
 use app\models\PsCommunityModel;
 
+use PHPUnit\Framework\Exception;
 use service\property_basic\JavaOfCService;
 use service\resident\MemberService;
 use service\property_basic\VoteService;
@@ -109,65 +110,83 @@ class VoteController extends BaseController
     // 投票接口
     public function actionDoVote()
     {
-        $voteId     = PsCommon::get($this->params, 'vote_id', 0);
-        $voteDetail = PsCommon::get($this->params, 'vote_det', '');
-        $memberId = PsCommon::get($this->params, 'member_id', 0);
-        $roomId = PsCommon::get($this->params, 'room_id', 0);
-        $token = PsCommon::get($this->params, 'token');
+        try{
 
-        if (!$voteId) {
-            return PsCommon::responseFailed('投票id必填');
-        }
+            $voteId     = PsCommon::get($this->params, 'vote_id', 0);
+            $voteDetail = PsCommon::get($this->params, 'vote_det', '');
+            $memberId = PsCommon::get($this->params, 'member_id', 0);
+            $roomId = PsCommon::get($this->params, 'room_id', 0);
+            $token = PsCommon::get($this->params, 'token');
 
-        if (!$memberId) {
-            return PsCommon::responseFailed('业主id不能为空');
-        }
-
-        if (!$roomId) {
-            return PsCommon::responseFailed('房间号id不能为空');
-        }
-
-        if (!$voteDetail) {
-            return PsCommon::responseFailed('请提交您的投票选项');
-        }
-
-        $problems = Yii::$app->db->createCommand("select id,option_type from ps_vote_problem where vote_id=:vote_id", [":vote_id" => $voteId])->queryAll();
-        $problem_type = array_column($problems, 'option_type', 'id');
-        $problem_ids = array_column($problems, 'id');
-
-        foreach ($voteDetail as $key => $det) {
-            if (!empty($det["problem_id"]) && in_array($det["problem_id"], $problem_ids)) {
-                if (empty($det["options"])) {
-                    return PsCommon::responseFailed('选项不能为空');
-                }
-                if (!$problem_type[$det["problem_id"]]) {
-                    return PsCommon::responseFailed('重复提交');
-                }
-                if (count($det["options"]) > 1 && $problem_type[$det["problem_id"]] == 1) {
-                    return PsCommon::responseFailed('单选问题答案不能多余1个');
-                }
-                unset($problem_type[$det["problem_id"]]);
-            } else {
-                return PsCommon::responseFailed('问题未找到');
+            if (!$voteId) {
+                return PsCommon::responseFailed('投票id必填');
             }
-        }
-        if (!empty($problem_type)) {
-            return PsCommon::responseFailed('问题未添加选项！');
-        }
 
-        $javaService = new JavaOfCService();
-        $javaParams['token'] = $token;
-        $javaResult = $javaService->memberBase($javaParams);
-        if(empty($javaResult)){
-            return PsCommon::responseFailed('用户不存在');
-        }
-        $doVote = VoteService::service()->doVote($voteId, $memberId, $javaResult['trueName'], $voteDetail, $this->params['community_id'], 'on', $roomId);
-        if ($doVote === true) {
-            return PsCommon::responseSuccess();
-        } elseif ($doVote === false){
-            return PsCommon::responseFailed('投票失败');
-        } else {
-            return PsCommon::responseFailed($doVote);
+            if (!$memberId) {
+                return PsCommon::responseFailed('业主id不能为空');
+            }
+
+            if (!$roomId) {
+                return PsCommon::responseFailed('房间号id不能为空');
+            }
+
+            if (!$voteDetail) {
+                return PsCommon::responseFailed('请提交您的投票选项');
+            }
+
+            $problems = Yii::$app->db->createCommand("select id,option_type from ps_vote_problem where vote_id=:vote_id", [":vote_id" => $voteId])->queryAll();
+            $problem_type = array_column($problems, 'option_type', 'id');
+            $problem_ids = array_column($problems, 'id');
+
+            foreach ($voteDetail as $key => $det) {
+                if (!empty($det["problem_id"]) && in_array($det["problem_id"], $problem_ids)) {
+                    if (empty($det["options"])) {
+                        return PsCommon::responseFailed('选项不能为空');
+                    }
+                    if (!$problem_type[$det["problem_id"]]) {
+                        return PsCommon::responseFailed('重复提交');
+                    }
+                    if (count($det["options"]) > 1 && $problem_type[$det["problem_id"]] == 1) {
+                        return PsCommon::responseFailed('单选问题答案不能多余1个');
+                    }
+                    unset($problem_type[$det["problem_id"]]);
+                } else {
+                    return PsCommon::responseFailed('问题未找到');
+                }
+            }
+            if (!empty($problem_type)) {
+                return PsCommon::responseFailed('问题未添加选项！');
+            }
+
+            $javaService = new JavaOfCService();
+            $javaParams['token'] = $token;
+            $javaResult = $javaService->memberBase($javaParams);
+            if(empty($javaResult)){
+                return PsCommon::responseFailed('用户不存在');
+            }
+
+            //获得投票信息
+            $voteDetail = Yii::$app->db->createCommand("select vote_name,community_id from ps_vote where id=:vote_id", [":vote_id" => $voteId])->queryOne();
+
+            $doVote = VoteService::service()->doVote($voteId, $memberId, $javaResult['trueName'], $voteDetail, $voteDetail['community_id'], 'on', $roomId);
+            if ($doVote === true) {
+
+                //添加会员积分
+//                $integralParams['token'] = $token;
+//                $integralParams['actKey'] = 'vote-publish';
+//                $integralParams['bizTitle'] = '参与投票：'.$voteDetail['vote_name'];
+//                $integralParams['communityId'] = $voteDetail['community_id'];
+//                $integralParams['memberId'] = $javaResult['id'];
+//                $javaService->integralGrant($integralParams);
+
+                return PsCommon::responseSuccess();
+            } elseif ($doVote === false){
+                return PsCommon::responseFailed('投票失败');
+            } else {
+                return PsCommon::responseFailed($doVote);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
         }
     }
 }
