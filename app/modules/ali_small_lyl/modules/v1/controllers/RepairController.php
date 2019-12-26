28256866a963f15b3aa2fc02f1fe1d908bee456c
wujianyang <wujianyang@zhujia360.com>
2019-12-26 17:27:22 +0800

    no message

--- app/modules/ali_small_lyl/modules/v1/controllers/RepairController.php
+++ app/modules/ali_small_lyl/modules/v1/controllers/RepairController.php
@@ -27,6 +27,7 @@ class RepairController extends BaseController
     public function actionCreate()
     {
         $p['community_id'] = F::value($this->params, 'community_id', 0);
+        $p['member_id'] = PsCommon::get($this->params, 'member_id', 0);
         $p['repair_type'] = F::value($this->params, 'repair_type_id', 0);
         $p['expired_repair_time'] = time();
         $p['expired_repair_type'] = F::value($this->params, 'expired_type', 0);
@@ -53,7 +54,6 @@ class RepairController extends BaseController
         }
         
         $p['contact_name'] = $member['trueName'];
-        $p['member_id'] = $member['id'];
 
         if ($relateRoom == 1) { // 1关联房屋
             $valid = PsCommon::validParamArr(new PsRepairRecord(), $p, 'add-repair3');
