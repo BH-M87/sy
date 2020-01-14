0faaee04f4d8d001eacc58a041f0bbae576c4706
chenkelang <chenkelang@zhujia360.com>
2020-01-14 15:04:38 +0800

    1

--- app/controllers/CommonController.php
+++ app/controllers/CommonController.php
@@ -63,7 +63,7 @@ class CommonController extends Controller
     //java透传接口
     public function actionPhpGateway()
     {
-        \Yii::info("PhpGateway-params:" . json_encode($this->params, JSON_UNESCAPED_UNICODE), 'api');
+        \Yii::info("PhpGateway-params-test:" . json_encode($this->params, JSON_UNESCAPED_UNICODE), 'api');
 
         $options['CURLOPT_HTTPHEADER'] = [
             "application/json;charset=UTF-8",
