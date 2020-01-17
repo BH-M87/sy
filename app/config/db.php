d9fc22903acbe8606d8f456bc864689decc82d9e
zhouph <zhoupenghui@zhujia360.com>
2020-01-17 18:13:53 +0800

    计划详情

--- app/config/db.php
+++ app/config/db.php
@@ -15,7 +15,7 @@ switch (YII_ENV) {
     case  "test"://测试环境
         return [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=rm-bp1up05n7675il875.mysql.rds.aliyuncs.com;dbname=property_sy_test',
+            'dsn' => 'mysql:host=rm-bp1up05n7675il875o.mysql.rds.aliyuncs.com;dbname=property_sy_test',
             'username' => 'shj',
             'password' => 'SHJ2017zhujia!@#',
             'charset' => 'utf8',
@@ -27,7 +27,7 @@ switch (YII_ENV) {
     case "dev"://本地开发环境
         return [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=192.168.1.112;dbname=property_sy_test',
+            'dsn' => 'mysql:host=rm-bp1up05n7675il875o.mysql.rds.aliyuncs.com;dbname=property_sy_test',
             'username' => 'root',
             'password' => 'zhujia360!@#',
             'charset' => 'utf8',
