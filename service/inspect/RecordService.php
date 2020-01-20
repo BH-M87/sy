5bd5f77fa5a4651e1ef2b2c5ecb329b4c80f904a
zhouph <zhoupenghui@zhujia360.com>
2020-01-20 16:32:24 +0800

    删除文件目录

--- service/inspect/RecordService.php
+++ service/inspect/RecordService.php
@@ -232,6 +232,7 @@ class RecordService extends BaseService {
             $downUrl = F::downloadUrl('record/' .  $file_name, 'zip');
             return PsCommon::responseSuccess(['down_url' => $downUrl]);
         } else {//下载zip压缩包
+            self::delDir($savePath);
             for ($i = 1; $i <= $cycle; $i++) {
                 $config["file_name"] = "Task".$i.".xlsx";
                 $params['page'] = $i;
@@ -247,6 +248,29 @@ class RecordService extends BaseService {
         }
     }
 
+    //清空文件夹函数和清空文件夹后删除空文件夹函数的处理
+    public function delDir($path){
+        //如果是目录则继续
+        if(is_dir($path)){
+            //扫描一个文件夹内的所有文件夹和文件并返回数组
+            $p = scandir($path);
+            foreach($p as $val){
+                //排除目录中的.和..
+                if($val !="." && $val !=".."){
+                    //如果是目录则递归子目录，继续操作
+                    if(is_dir($path.$val)){
+                        //子目录中操作删除文件夹和文件
+                        deldir($path.$val.'/');
+                        //目录清空后删除空文件夹
+                        @rmdir($path.$val.'/');
+                    }else{
+                        //如果是文件直接删除
+                        unlink($path.$val);
+                    }
+                }
+            }
+        }
+    }
     /*
      * 任务执行状态变化脚本
      */
