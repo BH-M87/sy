75cbc8af17c05b85770f5a66577ada3d116124fb
unknown <zhoupenghui@zhujia360.com>
2020-02-07 15:11:36 +0800

    desc update

--- app/models/PsInspectPlan.php
+++ app/models/PsInspectPlan.php
@@ -149,42 +149,42 @@ class PsInspectPlan extends BaseModel
             switch($this->exec_type){
                 case 1:     //天
                     if(!empty($this->exec_type_msg)){
-                        return $this->addError($attribute, "执行类型自定义为空");
+                        return $this->addError($attribute, "自定义日期为空");
                     }
                     break;
                 case 2:     //周
                     $temp = explode(",",$this->exec_type_msg);
                     if(empty($temp)){
-                        return $this->addError($attribute, "执行类型自定义格式错误");
+                        return $this->addError($attribute, "自定义日期不能为空");
                     }
                     foreach($temp as $value){
                         $value = intval($value);
                         if(!is_int($value)){
-                            return $this->addError($attribute, "执行类型自定义格式错误");
+                            return $this->addError($attribute, "自定义日期格式错误");
                         }
                         if($value<1||$value>7){
-                            return $this->addError($attribute, "执行类型自定义格式错误");
+                            return $this->addError($attribute, "自定义日期格式错误");
                         }
                     }
                     break;
                 case 3:     //月  32 默认最后一天
                     $temp = explode(",",$this->exec_type_msg);
                     if(empty($temp)){
-                        return $this->addError($attribute, "执行类型自定义格式错误");
+                        return $this->addError($attribute, "自定义日期不能为空");
                     }
                     foreach($temp as $value){
                         $value = intval($value);
                         if(!is_int($value)){
-                            return $this->addError($attribute, "执行类型自定义格式错误");
+                            return $this->addError($attribute, "自定义日期格式错误");
                         }
                         if($value<1||$value>32){
-                            return $this->addError($attribute, "执行类型自定义格式错误");
+                            return $this->addError($attribute, "自定义日期格式错误");
                         }
                     }
                     break;
                 case 4:     //年
                     if(!empty($this->exec_type_msg)){
-                        return $this->addError($attribute, "执行类型自定义为空");
+                        return $this->addError($attribute, "自定义日期为空");
                     }
                     break;
             }
