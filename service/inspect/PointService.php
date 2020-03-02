711864f96588d0edd9dc7b9df6ac2ca5ae4be088
wujianyang <wujianyang@zhujia360.com>
2020-03-02 15:28:52 +0800

    no message

--- service/inspect/PointService.php
+++ service/inspect/PointService.php
@@ -392,23 +392,25 @@ class PointService extends BaseService
             $point = PsInspectPoint::findOne($m['point_id']);
             $typeArr = explode(',', $point['type']);
 
-            if (in_array('1', $typeArr) && empty($p['sweepStatus'])) {
-                throw new MyException('该任务需扫码,扫码状态不能为空!');
+            if ($p['device_status'] == 1) { // 正常时需要判断 异常时可以不填
+                if (in_array('1', $typeArr) && empty($p['sweepStatus'])) {
+                    throw new MyException('该任务需扫码,扫码状态不能为空!');
+                }
+
+                if (in_array('2', $typeArr) && (empty($p['lat']) || empty($p['lon']) || empty($p['location']))) {
+                    throw new MyException('该任务需定位,经纬度不能为空!');
+                }
+
+                if (in_array('3', $typeArr) && empty($p['device_status'])) {
+                    throw new MyException('该任务需智点,智点状态不能为空!');
+                }
+
+                if (in_array('4', $typeArr) && empty($p['picture'])) {
+                    throw new MyException('该任务需拍照,图片不能为空!');
+                }
             }
 
-            if (in_array('2', $typeArr) && (empty($p['lat']) || empty($p['lon']) || empty($p['location']))) {
-                throw new MyException('该任务需定位,经纬度不能为空!');
-            }
-
-            if (in_array('3', $typeArr) && empty($p['device_status'])) {
-                throw new MyException('该任务需智点,智点状态不能为空!');
-            }
-
-            if (in_array('4', $typeArr) && empty($p['picture'])) {
-                throw new MyException('该任务需拍照,图片不能为空!');
-            }
-
-            if ($p['device_status'] == 2 && empty($p['record_note'])) {
+            if ($p['device_status'] == 2 && empty($p['record_note'])) { // 异常时必填
                 throw new MyException('巡检记录必填!');
             }
 
@@ -545,6 +547,7 @@ class PointService extends BaseService
         $query = new Query();
         $query->from('ps_inspect_record')->where("1=1")
             ->andfilterWhere(['user_id' => $p['user_id']])
+            ->andfilterWhere(['community_id' => $p['communityId']])
             ->andfilterWhere(['status' => [1,2]])
             ->andfilterWhere(['and', 
                 ['>=', 'check_start_at', $start], 
