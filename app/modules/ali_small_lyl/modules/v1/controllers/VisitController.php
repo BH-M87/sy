d48fe11c1623c24fb69c07e520d12cf77420d5e1
wujianyang <wujianyang@zhujia360.com>
2020-04-16 14:27:42 +0800

    no message

--- app/modules/ali_small_lyl/modules/v1/controllers/VisitController.php
+++ app/modules/ali_small_lyl/modules/v1/controllers/VisitController.php
@@ -10,6 +10,26 @@ use service\property_basic\JavaOfCService;
 
 class VisitController extends BaseController
 {
+    // 访客列表
+    public function actionList()
+    {
+        $r = VisitService::service()->list($this->params, $this->userInfo);
+        
+        return PsCommon::responseSuccess($r);
+    }
+
+    // 访客详情
+    public function actionShow()
+    {
+        if (!$this->params['id']) {
+            return F::apiFailed('请输入访客ID！');
+        }
+
+        $r = VisitService::service()->dingdingShow($this->params);
+
+        return PsCommon::responseSuccess($r);
+    }
+    
     // 访客新增
     public function actionAdd()
     {
