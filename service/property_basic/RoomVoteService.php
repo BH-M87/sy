0377279e2d229ce230c3e50d059c1c0586686baf
wujianyang <wujianyang@zhujia360.com>
2020-04-27 16:45:07 +0800

    no message

--- service/property_basic/RoomVoteService.php
+++ service/property_basic/RoomVoteService.php
@@ -70,16 +70,16 @@ class RoomVoteService extends BaseService
         $total = $arr['totalResident'];
         $total_9 = $total-$total_1-$total_2-$total_3;
 
-        $rate1 = $total > 0 ? round($total_1 / $total, 2) * 100 . '%' : '0%';
-        $rate2 = $total > 0 ? round($total_2 / $total, 2) * 100 . '%' : '0%';
-        $rate3 = $total > 0 ? round($total_3 / $total, 2) * 100 . '%' : '0%';
-        $rate9 = $total > 0 ? round($total_9 / $total, 2) * 100 . '%' : '0%';
+        $rate1 = $total > 0 ? round($total_1 / $total, 2) * 100 : 0;
+        $rate2 = $total > 0 ? round($total_2 / $total, 2) * 100 : 0;
+        $rate3 = $total > 0 ? round($total_3 / $total, 2) * 100 : 0;
+        $rate9 = 100 - $rate1 - $rate2 - $rate3;
 
         $r = [
-            ['type' => '赞成', 'rate' => $rate1, 'data' => $total_1, 'color' => '#1577FC'],
-            ['type' => '反对', 'rate' => $rate2, 'data' => $total_2, 'color' => '#F29927'],
-            ['type' => '弃权', 'rate' => $rate3, 'data' => $total_3, 'color' => '#F35A4C'],
-            ['type' => '未表态', 'rate' => $rate9, 'data' => $total_9, 'color' => '#9CA4BB']
+            ['type' => '赞成', 'rate' => $rate1 . '%', 'data' => $total_1, 'color' => '#1577FC'],
+            ['type' => '反对', 'rate' => $rate2 . '%', 'data' => $total_2, 'color' => '#F29927'],
+            ['type' => '弃权', 'rate' => $rate3 . '%', 'data' => $total_3, 'color' => '#F35A4C'],
+            ['type' => '未表态', 'rate' => $rate9 . '%', 'data' => $total_9, 'color' => '#9CA4BB']
         ];
 
         return $r;
@@ -102,16 +102,16 @@ class RoomVoteService extends BaseService
         $total = $arr['totalAreaSize'];
         $total_9 = $total-$total_1-$total_2-$total_3;
 
-        $rate1 = $total > 0 ? round($total_1 / $total, 2) * 100 . '%' : '0%';
-        $rate2 = $total > 0 ? round($total_2 / $total, 2) * 100 . '%' : '0%';
-        $rate3 = $total > 0 ? round($total_3 / $total, 2) * 100 . '%' : '0%';
-        $rate9 = $total > 0 ? round($total_9 / $total, 2) * 100 . '%' : '0%';
+        $rate1 = $total > 0 ? round($total_1 / $total, 2) * 100 : 0;
+        $rate2 = $total > 0 ? round($total_2 / $total, 2) * 100 : 0;
+        $rate3 = $total > 0 ? round($total_3 / $total, 2) * 100 : 0;
+        $rate9 = 100 - $rate1 - $rate2 - $rate3;
 
         $r = [
-            ['type' => '赞成', 'rate' => $rate1, 'data' => $total_1, 'color' => '#1577FC'],
-            ['type' => '反对', 'rate' => $rate2, 'data' => $total_2, 'color' => '#F29927'],
-            ['type' => '弃权', 'rate' => $rate3, 'data' => $total_3, 'color' => '#F35A4C'],
-            ['type' => '未表态', 'rate' => $rate9, 'data' => $total_9, 'color' => '#9CA4BB']
+            ['type' => '赞成', 'rate' => $rate1 . '%', 'data' => $total_1, 'color' => '#1577FC'],
+            ['type' => '反对', 'rate' => $rate2 . '%', 'data' => $total_2, 'color' => '#F29927'],
+            ['type' => '弃权', 'rate' => $rate3 . '%', 'data' => $total_3, 'color' => '#F35A4C'],
+            ['type' => '未表态', 'rate' => $rate9 . '%', 'data' => $total_9, 'color' => '#9CA4BB']
         ];
 
         return $r;
