aeb0faed609d3b7050b581253d2ce7b9dffc8c78
zhouph <zhoupenghui@zhujia360.com>
2020-05-18 11:54:09 +0800

    降级处理

--- app/modules/ding_property_app/config/downgrade.php
+++ app/modules/ding_property_app/config/downgrade.php
@@ -1,12 +1,12 @@
 <?php
 $params = [
-    'inspect_list' => false,                //巡检执行入口
-    'inspect_point' => false,               //巡检点入口
-    'inspect_line_list' => false,           //巡检路线列表
-    'inspect_plan_list'=>false,             //巡检计划列表
-    'inspect_record_list' => false,         //巡检任务列表
-    'inspect_device_list' => false,         //巡检设备列表
-    'repair_type' => false,         //报事保修
+    'inspect_list' => true,                //巡检执行入口
+    'inspect_point' => true,               //巡检点入口
+    'inspect_line_list' => true,           //巡检路线列表
+    'inspect_plan_list'=>true,             //巡检计划列表
+    'inspect_record_list' => true,         //巡检任务列表
+    'inspect_device_list' => true,         //巡检设备列表
+    'repair_type' => true,                 //报事保修
     'msg' => '模块维护中...',
 ];
 return $params;
