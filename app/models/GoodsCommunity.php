58bafc81e9bca96eb8c099d2ca83280becc02393
wujianyang <wujianyang@zhujia360.com>
2020-05-22 15:08:30 +0800

    兑换管理

---
+++ app/models/GoodsCommunity.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\models;
+
+use Yii;
+
+class GoodsCommunity extends BaseModel
+{
+    public static function tableName()
+    {
+        return 'ps_goods_community';
+    }
+
+    public function rules()
+    {
+        return [
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+        ];
+    }
+}
