a4cee63a7a86148449eb2dcae26ce862fdf0acab
zhouph <zhoupenghui@zhujia360.com>
2020-05-27 17:14:52 +0800

    街道接口调用

--- app/modules/ali_small_lyl/config/params.php
+++ app/modules/ali_small_lyl/config/params.php
@@ -5,7 +5,7 @@ $params = [
 switch (YII_ENV) {
     case  "master":
         //志愿者域名
-        $params['volunteer_host'] = 'https://dev-api.elive99.com/volunteer-zhd/?r=';
+        $params['volunteer_host'] = 'https://wmdn-api.zje.com/index.php?r=';
         break;
     case  "test":
         //志愿者域名
