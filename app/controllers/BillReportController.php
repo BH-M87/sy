6da778c460104cbec6f838903060280a835e4a10
zhouph <zhoupenghui@zhujia360.com>
2020-06-02 11:21:06 +0800

    账单列表

--- app/controllers/BillReportController.php
+++ app/controllers/BillReportController.php
@@ -13,6 +13,37 @@ use Yii;
 Class BillReportController extends Controller
 {
 
+    /**
+     * 账单表拆统计基础表（一次性方法）
+     * @author yjh
+     * @return void
+     * @throws redis记录
+     */
+    public function actionSplitBill()
+    {
+        $flag = true;
+        $id = 0;
+        $i = 1000;
+        while ($flag) {
+            $bill_param['row'] =  200;
+            $bill_param['where'] =  ['>','id', $id];
+            $bill_param['andwhere'] =  ['status'=>[1,2,7],'is_del'=>1,'trade_defend'=>0];//查询未删除数据并且是支付类型
+            $bill_param['field'] =  'room_id,id,community_id,,order_id,bill_entry_id,acct_period_start,acct_period_end,cost_id,bill_entry_amount,paid_entry_amount,prefer_entry_amount,status,trade_type,is_del';
+            $info =  BillTractContractService::service()->getList('bill',$bill_param);
+            if (!empty($info)) {
+                foreach ($info as $v) {
+                    BillTractContractService::service()->splitBill($v);
+                }
+                $id = end($info)['id'];
+                Yii::$app->redis->set('split_bill_log', 'complete:'.$i*$bill_param['row'].'||running_id:'.$id);
+                $i++;
+            } else {
+                $flag = false;
+            }
+        }
+        Yii::$app->redis->set('split_bill_log', 'complete'.$i*$bill_param['row'].'||END');
+    }
+
     /*统计账单数据*/
     public function actionBillAddReportNew()
     {
