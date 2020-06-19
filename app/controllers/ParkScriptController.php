fcdd5e568106b2beda5eaf865a2f0fdd60636ca4
zhouph <zhoupenghui@zhujia360.com>
2020-06-19 09:28:07 +0800

    脚本文件

---
+++ app/controllers/ParkScriptController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\controllers;
+
+
+use yii\base\Controller;
+
+class ParkScriptController extends Controller  {
+
+
+    //车辆出入场
+    public function actionCarEntryExit(){
+
+
+    }
+
+}
