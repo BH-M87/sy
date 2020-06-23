614d2b9d5c6aa77b1a85041d6f8e04b55bd137a1
chenkelang <chenkelang@zhujia360.com>
2020-06-23 17:26:52 +0800

    1

--- app/models/PsParkReservation.php
+++ app/models/PsParkReservation.php
@@ -253,11 +253,15 @@ class PsParkReservation extends BaseModel
                 $housTo = $over_time>60?intval($over_time/60):'';//大与60分钟显示小时+分钟
                 if(!empty($housTo)){
                     $over_time = $housTo."小时".($over_time - $housTo*60).'分钟';
+                }else{
+                    $over_time = $over_time.'分钟';
                 }
             }
             $hous = $usage_time>60?intval($usage_time/60):'';//大与60分钟显示小时+分钟
             if(!empty($hous)){
                 $usage_time = $hous."小时".($usage_time - $hous*60).'分钟';
+            }elese{
+                $usage_time = $usage_time.'分钟';
             }
             $data['usage_time'] = $usage_time;
             $data['over_time'] = !empty($over_time)?$over_time:0;
