6414f4b0409e92c4b39e281afd1fd21cbf3392f4
zhouph <zhoupenghui@zhujia360.com>
2020-07-07 15:57:47 +0800

    商户

---
+++ app/modules/ali_small_lyl/modules/v1/controllers/ShopMerchantController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouph
+ * Date: 2020/7/7
+ * Time: 15:53
+ * Desc: 商户
+ */
+namespace app\modules\ali_small_lyl\modules\v1\controllers;
+
+use app\modules\ali_small_lyl\controllers\BaseController;
+use common\core\PsCommon;
+use service\shop\MerchantService;
+use yii\base\Exception;
+
+class ShopMerchantController extends BaseController{
+
+    //商户入驻
+    public function actionAdd(){
+        try{
+            $result = MerchantService::service()->addOfC($this->params);
+            if ($result['code']) {
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
+        }
+    }
+}
---
+++ service/shop/MerchantService.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouph
+ * Date: 2020/7/7
+ * Time: 15:55
+ * Desc: 商户service
+ */
+namespace service\shop;
+
+use service\BaseService;
+
+Class MerchantService extends BaseService {
+
+    //商户入驻
+    public function addOfC($params){
+
+    }
+}
