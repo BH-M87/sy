14330ea502899793622700038e085ff56b631e94
zhouph <zhoupenghui@zhujia360.com>
2020-07-10 15:24:11 +0800

    excel导出

--- app/modules/operation/modules/v1/controllers/ShopMerchantController.php
+++ app/modules/operation/modules/v1/controllers/ShopMerchantController.php
@@ -9,9 +9,12 @@
 namespace app\modules\operation\modules\v1\controllers;
 
 use app\modules\operation\controllers\BaseController;
+use common\core\F;
+use service\common\ExcelService;
 use service\shop\MerchantService;
 use yii\base\Exception;
 use common\core\PsCommon;
+use Yii;
 
 class ShopMerchantController extends BaseController {
 
@@ -126,7 +129,60 @@ class ShopMerchantController extends BaseController {
     /*
      * 商家导出
      */
-    public function action(){
+    public function actionMerchantExport(){
+        try{
+            $params = $this->request_params;
+            $service = new MerchantService();
+            $result = $service->merchantList($params);
+            if ($result['code']) {
+                $getTotals = $result['data']['totals'];
+                if ($getTotals > 0) {
 
+                    $cycle = ceil($getTotals / 1000);
+                    $config["sheet_config"] = [
+
+                        'A' => ['title' => '商家ID', 'width' => 20, 'data_type' => 'str', 'field' => 'merchant_code'],
+                        'B' => ['title' => '商家名称', 'width' => 25, 'data_type' => 'str', 'field' => 'name'],
+                        'C' => ['title' => '商家类型', 'width' => 15, 'data_type' => 'str', 'field' => 'type_msg'],
+                        'D' => ['title' => '店铺数', 'width' => 10, 'data_type' => 'str', 'field' => 'count'],
+                        'E' => ['title' => '商家状态', 'width' => 10, 'data_type' => 'str', 'field' => 'status_msg'],
+                        'F' => ['title' => '入驻时间', 'width' => 25, 'data_type' => 'str', 'field' => 'create_at_msg'],
+                    ];
+                    $config["save"] = true;
+                    $date = date('Y-m-d',time());
+                    $savePath = Yii::$app->basePath . '/web/store/zip/shop/' . $date . '/';
+                    $config["save_path"] = $savePath;
+                    //房屋数量查过一千则导出压缩文件
+                    if ($cycle == 1) {//下载单个文件
+                        $config["file_name"] = "MuBan1.xlsx";
+                        $params['page'] = 1;
+                        $params['pageSize'] = 1000;
+                        $result = $service->merchantList($params);
+                        $file_name = ExcelService::service()->recordDown($result['data']['list'], $config);
+                        $downUrl = F::downloadUrl('shop/' . $date . '/'. $file_name, 'zip');
+                        return PsCommon::responseSuccess(['down_url' => $downUrl]);
+                    } else {//下载zip压缩包
+                        for ($i = 1; $i <= $cycle; $i++) {
+                            $config["file_name"] = "MuBan" . $i . ".xlsx";
+                            $params['page'] = $i;
+                            $params['pageSize'] = 1000;
+                            $result = $service->merchantList($params);
+                            $config["file_name"] = "MuBan" . $i . ".xlsx";
+                            ExcelService::service()->recordDown($result['data']['list'], $config);
+                        }
+                        $path = $savePath . 'merchant.zip';
+                        ExcelService::service()->addZip($savePath, $path);
+                        $downUrl = F::downloadUrl('shop/'.$date.'/merchant.zip', 'zip');
+                        return PsCommon::responseSuccess(['down_url' => $downUrl]);
+                    }
+                } else {
+                    return PsCommon::responseFailed("暂无数据！");
+                }
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch(Exception $e){
+            return PsCommon::responseFailed($e->getMessage());
+        }
     }
 }
