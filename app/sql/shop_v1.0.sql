1bd190a86ced37497790631d7800f72be844e6ce
wujianyang <wujianyang@zhujia360.com>
2020-07-16 16:10:05 +0800

    no message

--- app/sql/shop_v1.0.sql
+++ app/sql/shop_v1.0.sql
@@ -166,3 +166,13 @@ CREATE TABLE `ps_shop_merchant_promote` (
   `update_at` int(10) NOT NULL DEFAULT 0 COMMENT '修改时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='社区店铺推广';
+
+CREATE TABLE `ps_shop_statistic` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `shop_id` int(10) NOT NULL COMMENT '店铺id',
+  `year` int(11) NOT NULL COMMENT '统计时所在年份',
+  `month` tinyint(2) NOT NULL COMMENT '任务所在月份',
+  `day` date NOT NULL COMMENT '任务在具体的哪天',
+  `click_num` int(10) NOT NULL DEFAULT '0' COMMENT '今天的点击数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='店铺点击统计表';
