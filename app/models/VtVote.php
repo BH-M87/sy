c875b6a9c5c4bd2ff71a33238a8dd10b7ee7d1d0
wujianyang <wujianyang@zhujia360.com>
2020-07-23 11:30:36 +0800

    1

---
+++ app/models/VtVote.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\models;
+
+use Yii;
+use common\core\Regular;
+
+class VtVote extends BaseModel
+{
+    public static function tableName()
+    {
+        return 'vt_vote';
+    }
+
+    public function rules()
+    {
+        return [
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+        ];
+    }
+
+     // 新增 编辑
+    public function saveData($scenario, $p)
+    {
+        if ($scenario == 'edit') {
+            $p['update_at'] = time();
+            self::updateAll($p, ['id' => $p['id']]);
+            return true;
+        }
+        return $this->save();
+    }
+}
