b509f2c2ecec588be726e5b12951e57a96ca04c7
zhouph <zhoupenghui@zhujia360.com>
2020-07-23 11:49:15 +0800

    活动新增

---
+++ app/models/VtActivity.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouph
+ * Date: 2020/7/23
+ * Time: 11:19
+ * Desc: 活动
+ */
+namespace app\models;
+
+class VtActivity extends BaseModel
+{
+
+
+    public static function tableName()
+    {
+        return 'vt_activity';
+    }
+
+    public function rules()
+    {
+        return [
+
+            [['name', 'code', 'start_at', 'end_at', 'group_status'], 'required', 'message' => '{attribute}不能为空！', 'on' => ['add']],
+            [['id','name', 'start_at', 'end_at', 'group_status'], 'required', 'message' => '{attribute}不能为空！', 'on' => ['edit']],
+            [['id'], 'required', 'message' => '{attribute}不能为空！', 'on' => ['detail']],
+            [["id",  'start_at', 'end_at', 'group_status', 'create_at', 'update_at'], 'integer'],
+            [['name', 'code', 'start_at', 'end_at', 'group_status','content'], 'trim'],
+            [['content'], 'string'],
+            [['code'], 'string', "max" => 20],
+            [['name'], 'string', "max" => 50],
+            [['link_url', 'qrcode'], 'string', "max" => 255],
+            ['group_status', 'in', 'range' => [1, 2], 'on' => ['add','edit']],
+            [['code'], 'codeInfo', 'on' => ["add"]], //活动code唯一
+            [['start_at', 'end_at'], 'timeVerification', 'on' => ["add","edit"]], //活动code唯一
+            [["create_at", 'update_at'], "default", 'value' => time(), 'on' => ['add']],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+              'id'              => '活动id',
+              'code'            => '活动code',
+              'name'            => '活动名称',
+              'start_at'        => '开始时间',
+              'end_at'          => '结束时间',
+              'content'         => '活动规则',
+              'group_status'    => '选手分组 1启用 2禁用',
+              'link_url'        => '页面链接',
+              'qrcode'          => '二维码',
+              'create_at'       => '创建时间',
+              'update_at'       => '修改时间',
+        ];
+    }
+
+    /***
+     * 新增
+     * @return true|false
+     */
+    public function saveData()
+    {
+        return $this->save();
+    }
+
+    /***
+     * 修改
+     * @return bool
+     */
+    public function edit($param)
+    {
+        $param['update_at'] = time();
+        return self::updateAll($param, ['id' => $param['id']]);
+    }
+
+    /*
+     * code唯一
+     */
+    public function codeInfo($attribute){
+        if(!empty($this->code)){
+            $res = self::find()->select(['id'])->where(['=','code',$this->code])->asArray()->one();
+            if(!empty($res)){
+                return $this->addError($attribute, "投票活动code已存在，请重新输入");
+            }
+        }
+    }
+
+    /*
+     * 时间验证
+     */
+    public function timeVerification($attribute){
+        if(!empty($this->start_at)&&!empty($this->end_at)){
+            $nowTime = time();
+            $this->start_at = strtotime($this->start_at);
+            $this->end_at = strtotime($this->end_at);
+            if($this->start_at<$nowTime){
+                return $this->addError($attribute, "投票活动开始时间应大于当前时间");
+            }
+
+            if($this->start_at>$this->end_at){
+                return $this->addError($attribute, "投票活动开始时间应小于投票结束时间");
+            }
+        }
+    }
+}
--- service/vote/ActivityService.php
+++ service/vote/ActivityService.php
@@ -8,6 +8,7 @@
  */
 namespace service\vote;
 
+use app\models\VtActivity;
 use service\BaseService;
 use Yii;
 use yii\db\Exception;
@@ -16,7 +17,31 @@ Class ActivityService extends BaseService {
 
     //新建活动
     public function add($params){
+        $trans = Yii::$app->db->beginTransaction();
+        try{
+            $addParams['code'] = !empty($params['code'])?$params['code']:'';
+            $addParams['name'] = !empty($params['name'])?$params['name']:'';
+            $addParams['start_at'] = !empty($params['start_at'])?$params['start_at']:'';
+            $addParams['end_at'] = !empty($params['end_at'])?$params['end_at']:'';
+            $addParams['content'] = !empty($params['content'])?$params['content']:'';
+            $addParams['group_status'] = !empty($params['group_status'])?$params['group_status']:'';
 
+            $model = new VtActivity(['scenario'=>'add']);
+            if($model->load($addParams,'')&&$model->validate()){
+                if(!$model->saveData()){
+                    throw new Exception('活动新建失败！');
+                }
+
+                $trans->commit();
+                return $this->success(['id'=>$model->attributes['id']]);
+            }else{
+                $msg = array_values($model->errors)[0][0];
+                throw new Exception($msg);
+            }
+        }catch (Exception $e) {
+            $trans->rollBack();
+            return $this->failed($e->getMessage());
+        }
     }
 
 }
