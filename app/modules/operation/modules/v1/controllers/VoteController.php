2c9af1f271ad1bcfde74661d2d028e68ee4c673f
zhouph <zhoupenghui@zhujia360.com>
2020-07-23 11:12:12 +0800

    service

---
+++ app/modules/operation/modules/v1/controllers/VoteController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouph
+ * Date: 2020/7/23
+ * Time: 11:08
+ * Desc: 投票
+ */
+namespace app\modules\operation\modules\v1\controllers;
+
+use app\modules\operation\controllers\BaseController;
+use yii\base\Exception;
+use common\core\PsCommon;
+use Yii;
+
+class VoteController extends BaseController {
+
+}
---
+++ service/vote/VoteService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhouph
+ * Date: 2020/7/23
+ * Time: 11:09
+ * Desc: 投票
+ */
+namespace service\shop;
+
+use service\BaseService;
+use Yii;
+use yii\db\Exception;
+
+Class VoteService extends BaseService {
+
+}
