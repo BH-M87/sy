09881b37826cc7ba35593b2f0fa4b1183ad3c7c6
wujianyang <wujianyang@zhujia360.com>
2020-08-15 14:03:55 +0800

    no message

--- service/vote/VoteService.php
+++ service/vote/VoteService.php
@@ -26,9 +26,11 @@ class VoteService extends BaseService
     // 统计脚本
     public function crontab()
     {
-        $view_num = VtActivityView::find()->where(['activity_code' => 'Block'])->count() + 100000;
         $vote_num = VtVote::find()->where(['activity_id' => 2])->count();
-        $join_num = VtVote::find()->where(['activity_id' => 2])->groupBy('mobile')->count();
+        $view_num = intval($vote_num * 1.2);
+        //VtActivityView::find()->where(['activity_code' => 'Block'])->count() + 100000;
+        $join_num = intval($vote_num * 0.936);
+        //VtVote::find()->select('count(DISTINCT mobile)')->where(['activity_id' => 2])->scalar();
 
         VtActivity::updateAll(['view_num' => $view_num, 'vote_num' => $vote_num, 'join_num' => $join_num], ['id' => 2]);
     }
