87f79dd556075da0a0f2d17ab60fd1020857420e
zhouph <zhoupenghui@zhujia360.com>
2020-08-20 15:44:52 +0800

    出门单

--- service/visit/OutOrderService.php
+++ service/visit/OutOrderService.php
@@ -16,20 +16,20 @@ class OutOrderService extends BaseService{
     public function addOfC($params){
         $recordsParams['community_id'] = !empty($params['community_id'])?$params['community_id']:'';
         $recordsParams['community_name'] = !empty($params['community_name'])?$params['community_name']:'';
-        $recordsParams['groupId'] = !empty($params['groupId'])?$params['groupId']:'';
-        $recordsParams['buildingId'] = !empty($params['buildingId'])?$params['buildingId']:'';
-        $recordsParams['unitId'] = !empty($params['unitId'])?$params['unitId']:'';
-        $recordsParams['roomId'] = !empty($params['roomId'])?$params['roomId']:'';
+        $recordsParams['group_id'] = !empty($params['group_id'])?$params['group_id']:'';
+        $recordsParams['building_id'] = !empty($params['building_id'])?$params['building_id']:'';
+        $recordsParams['unit_id'] = !empty($params['unit_id'])?$params['unit_id']:'';
+        $recordsParams['room_id'] = !empty($params['room_id'])?$params['room_id']:'';
         $recordsParams['application_name'] = !empty($params['application_name'])?$params['application_name']:'';
         $recordsParams['application_mobile'] = !empty($params['application_mobile'])?$params['application_mobile']:'';
-        $recordsParams['member_type'] = !empty($params['member_type'])?$params['member_type']:'';
+        $recordsParams['member_type'] = !empty($params['member_type'])?$params['member_type']:'1';
         $recordsParams['room_address'] = !empty($params['room_address'])?$params['room_address']:'';
         $recordsParams['application_id'] = !empty($params['application_id'])?$params['application_id']:''; //会员member
         $recordsParams['application_at'] = !empty($params['application_at'])?strtotime($params['application_at']." 23:59:59"):'';
         $recordsParams['content'] = !empty($params['content'])?$params['content']:'';
         $recordsParams['car_number'] = !empty($params['car_number'])?$params['car_number']:'';
         $recordsParams['content_img'] = !empty($params['content_img'])?$params['content_img']:'';
-        $recordsParams['ali_form_id'] = !empty($params['ali_form_id'])?$params['ali_form_id']:'';
+        $recordsParams['ali_form_id'] = !empty($params['ali_form_id'])?$params['ali_form_id']:'456';
         $recordsParams['ali_user_id'] = !empty($params['ali_user_id'])?$params['ali_user_id']:'';
         $model = new PsOutOrder(['scenario'=>'add']);
         if($model->load($recordsParams,'')&&$model->validate()) {
