5cb6a6e58ea6ecbc5a4cb045878c3d15c7719afa
chenkelang <chenkelang@zhujia360.com>
2020-08-25 11:52:57 +0800

    1

--- service/property_basic/JavaService.php
+++ service/property_basic/JavaService.php
@@ -222,11 +222,11 @@ class JavaService extends BaseService
         $query['route'] = '/unit/unitTree';
         $result = self::returnCData($query);
         if(!empty($result['list'])){
-            foreach($result['list'] as $key=>$value){
+            foreach($result['list'] as $key=>&$value){
                 $value['title'] = $value['name'];
                 $value['value'] = $value['id'];
                 if(!empty($value['children'])){
-                    foreach($value['children'] as $ck=>$cv){
+                    foreach($value['children'] as $ck=>&$cv){
                         $cv['title'] = $cv['name'];
                         $cv['value'] = $cv['id'];
                         if(!empty($cv['children'])){
