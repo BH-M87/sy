835bd8b77df69dc423beaa85ce865ee190c61912
wujianyang <wujianyang@zhujia360.com>
2020-08-25 09:29:55 +0800

    no message

--- service/visit/VisitService.php
+++ service/visit/VisitService.php
@@ -192,6 +192,9 @@ class VisitService extends BaseService
                     'code' => rand(100,999).rand(100,999),
                 ], ['id' => $id]);
 
+                $data['keyword1'] = ['value'=>'已确认'];
+                $data['keyword2'] = ['value'=>'2'];
+                $data['keyword3'] = ['value'=>'3'];
                 $data['keyword4'] = ['value'=>$r['application_name']];
                 $params['to_user_id'] = $r['ali_user_id'];
                 $params['form_id'] = $r['ali_form_id'];
