c546ae9667ee6abce9a24323ef3057fc79238718
chenkelang <944344874@qq.com>
2020-10-13 15:51:12 +0800

    独立水表电表导入

--- app/modules/property/modules/v1/controllers/MeterController.php
+++ app/modules/property/modules/v1/controllers/MeterController.php
@@ -10,7 +10,11 @@ namespace app\modules\property\modules\v1\controllers;
 use app\modules\property\controllers\BaseController;
 use common\core\F;
 use common\core\PsCommon;
+use service\alipay\ElectrictMeterService;
 use service\alipay\MeterService;
+use service\alipay\WaterMeterService;
+use service\common\ExcelService;
+use service\rbac\OperateService;
 use Yii;
 
 class MeterController extends BaseController
@@ -64,6 +68,72 @@ class MeterController extends BaseController
         return PsCommon::responseSuccess(['down_url' => $downUrl]);
     }
 
+    /*
+     * 水表电表 导入
+     */
+    public function actionImport()
+    {
+        $community_id = PsCommon::get($this->request_params, 'community_id');
+        $type = PsCommon::get($this->request_params, 'type');
+        if (!$community_id) {
+            return PsCommon::responseAppFailed("未获得小区");
+        }
+        if(!$type){
+            return PsCommon::responseAppFailed("电表类型必填");
+        }
+        $file = $_FILES["file"];
+        $savePath = F::excelPath('water_meter');
+        $excel_upload = ExcelService::service()->excelUpload($file, $savePath);
 
+        if (!$excel_upload["status"]) {
+            return PsCommon::responseAppFailed($excel_upload["errorMsg"]);
+        }
+        if ($excel_upload["data"]["totals"] < 3) {
+            return PsCommon::responseAppFailed("未检测到有效数据");
+        }
+        if ($excel_upload["data"]["totals"] >= 1003) {
+            return PsCommon::responseAppFailed("只能添加1000条数据");
+        }
+        $typefile = $savePath . $excel_upload["data"]['next_name'];
+        $inputFileType = \PHPExcel_IOFactory::identify($typefile);
+        $objReader = \ PHPExcel_IOFactory::createReader($inputFileType);
+        $objReader->setReadDataOnly(true);
+        $PHPExcel = $objReader->load($typefile);
+        $currentSheet = $PHPExcel->getActiveSheet();
+        $sheetData = $currentSheet->toArray(null, false, false, true);
+        if (empty($sheetData)) {
+            return PsCommon::responseAppFailed('表格里面为空');
+        }
+        if($type==1){
+            $result = WaterMeterService::service()->import($sheetData, $community_id, $this->user_info,$this->request_params);
+            if ($result['code']) {
+                $operate = [
+                    "community_id" => $community_id,
+                    "operate_menu" => "水表管理",
+                    "operate_type" => "水表导入",
+                    "operate_content" => "",
+                ];
+                OperateService::addComm($this->user_info, $operate);
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }else{
+            $result = ElectrictMeterService::service()->import($sheetData, $community_id, $this->user_info,$this->request_params);
+            if ($result['code']) {
+                $operate = [
+                    "community_id" => $community_id,
+                    "operate_menu" => "电表管理",
+                    "operate_type" => "电表导入",
+                    "operate_content" => "",
+                ];
+                OperateService::addComm($this->user_info, $operate);
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }
+
+    }
 
 }
