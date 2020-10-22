c1f8d0bc3d8415f3ae00c1c9890112d0c938fd73
chenkelang <944344874@qq.com>
2020-10-22 09:49:13 +0800

    装修登记

---
+++ app/modules/property/modules/v1/controllers/DecorationController.php
@@ -0,0 +1,71 @@
+<?php
+namespace app\modules\property\modules\v1\controllers;
+
+use app\modules\property\controllers\BaseController;
+use common\core\PsCommon;
+use service\property_basic\DecorationService;
+use yii\base\Exception;
+
+class DecorationController extends BaseController{
+
+    //装修登记-新增
+    public function actionAdd(){
+        try{
+            $result = DecorationService::service()->add($this->request_params,$this->user_info);
+            if ($result['code']) {
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
+        }
+    }
+
+    //装修登记-列表
+    public function actionList(){
+        try{
+            $params = $this->request_params;
+            $params['page'] = $this->page;
+            $params['pageSize'] = $this->pageSize;
+            $result = DecorationService::service()->getList($params);
+            if ($result['code']) {
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
+        }
+    }
+
+    //装修登记-详情
+    public function actionDetail(){
+        try{
+            $params = $this->request_params;
+            $result = DecorationService::service()->getDetail($params);
+            if ($result['code']) {
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
+        }
+    }
+
+    //装修登记-完成
+    public function actionComplete(){
+        try{
+            $params = $this->request_params;
+            $result = DecorationService::service()->complete($params,$this->user_info);
+            if ($result['code']) {
+                return PsCommon::responseSuccess($result['data']);
+            } else {
+                return PsCommon::responseFailed($result['msg']);
+            }
+        }catch (Exception $e){
+            exit($e->getMessage());
+        }
+    }
+}
